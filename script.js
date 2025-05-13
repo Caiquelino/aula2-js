ed044e41cb2a205f06a5f69105b3d1ddc0a3fcfe
CAIQUE BATISTA LINO <caiquelino6183@digitalcollege.internal>
2025-05-13 19:54:42 -0300

    Comitando dois exercicios em JS AULA 02

---
+++ script.js
@@ -0,0 +1,15 @@
+// let nome = prompt("qual é o seu nome?")
+
+// let idade = prompt("quantos anos você tem?")
+
+// alert(`Olá, ${nome}! você tem ${idade} anos!`)
+
+// confirm()
+
+// let resposta = confirm("você tem certeza que deseja sair?")
+// console.log(resposta)
+
+const nomeDoUsuario = prompt("Qual é o seu nome?")
+const profissaoDoUsuario = prompt("Qual é a sua profissão?")
+
+alert(`Olá, ${nomeDoUsuario}! que interessante saber que você trabalha com ${profissaoDoUsuario}.`)
